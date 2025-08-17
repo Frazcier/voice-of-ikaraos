e28343e991e9faa47b57042c339cdc6c8c82f76d
Frazcier <abat.arian13@gmail.com>
2025-08-17 23:32:45 +0800

    tweaking

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -14,4 +14,11 @@ window.addEventListener("scroll", () => {
   else {
     header.classList.remove("scrolled");
   }
-})
+});
+
+const navToggle = document.getElementById("navToggle");
+document.querySelectorAll("header .col-2 .nav-item").forEach(label => {
+  label.addEventListener("click", () => {
+    navToggle.checked = false;
+  });
+});
